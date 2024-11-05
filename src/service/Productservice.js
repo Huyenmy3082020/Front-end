5dde758b613584a4f836ac1abee3d3e10e719efc
Huyenmy30082020 <Hatuan1562004@gmail.com>
2024-11-05 23:33:14 +0700

    a

--- src/service/Productservice.js
+++ src/service/Productservice.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Tạo instance của axios với URL từ biến môi trường
 const axiosInstance = axios.create({
-    baseURL: 'https://alo-8.onrender.com',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const getAllProduct = async (limit) => {
